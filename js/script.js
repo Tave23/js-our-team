c2ad50058ea426062af8a8dfe7126455313617db
Davide Tavella <tavella01@gmail.com>
2021-11-04 16:07:26 +0100

    bug immagini

--- js/script.js
+++ js/script.js
@@ -33,6 +33,59 @@ const cards = [
       
 ];
 
-console.log(cards);
-
 // stampiamo le card del team (se ci riusciamo)
+
+
+
+// funzione per creare nuove card
+
+newCard();
+
+
+function newCard() {
+   // ciclo for per prendere tutti i dati
+   for(let i = 0; i < cards.length; i++){
+      // intero object
+      const singleCard = cards[i]
+      // console.log(singleCard);
+      // nome
+      const nameCard = singleCard.name;
+      console.log(nameCard);
+      // ruolo
+      const roleCard = singleCard.role;
+      console.log(roleCard);
+      // image
+      const imageCard = singleCard.image;
+      console.log(imageCard);
+
+
+      // prendo team-container e ci creo un altro team-card
+      const container = document.querySelector('.team-container');
+      // ci creo un'altro div al suo interno (team-card)
+      const newTeamCard = document.createElement('div');
+      // gli dò la classe team-card
+      newTeamCard.classList.add('team-card');
+      // inserisco newteam card dentro container
+      container.append(newTeamCard);
+      console.log(container);
+      // dentro al div team-card inserisco l'immagine
+      // creo il div card-image con la sua relativa classe
+      const newCardImage = document.createElement('div');
+      // aggiungo classe
+      newCardImage.classList.add('card-image');
+      // inserisco newcard card dentro container
+      newTeamCard.append(newCardImage);
+      console.log(newTeamCard);
+      console.log(newCardImage);
+
+      // inserisco le immagini
+      newCardImage.append(imageCard);
+      
+      // parte testuale
+      const newCardImage = document.createElement('div');
+   }
+
+}
+
+// newCardImage.innerHTML = `<img src="img/${imageCard}"
+//       // alt="${nameCard}"/>`;
