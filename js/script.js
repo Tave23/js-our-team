ced9de709217e140d2766de5b98c2dce0a567a1f
Davide Tavella <tavella01@gmail.com>
2021-11-04 16:33:09 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -9,83 +9,63 @@
 
 // creo array di oggetti di ciascun membro
 
+const container = document.querySelector('.team-container');
+let newHtml = '';
+
 const cards = [
+// membro 1
+   {name:"wayne Barnett", 
+   role:"Founder & CEO", 
+   image:"wayne-barnett-founder-CEO.jpg"},
 // membro 2
    {name:"Angela Carroll", 
    role:"Chief Editor", 
-   image:"img/angela-caroll-chief-editor.jpg"},
+   image:"angela-caroll-chief-editor.jpg"},
 // membro 3
    {name:"Walter Gordon", 
    role:"Office Manager", 
-   image:"img/walter-gordon-office-manager.jpg"},
+   image:"walter-gordon-office-manager.jpg"},
 // membro 4
    {name:"Angela Lopez", 
    role:"Social Media Manager", 
-   image:"img/angela-lopez-social-media-manager.jpg"},
+   image:"angela-lopez-social-media-manager.jpg"},
 // membro 5
    {name:"Scott Estrada", 
    role:"Developer", 
-   image:"img/scott-estrada-developer.jpg"},
+   image:"scott-estrada-developer.jpg"},
 // membro 6
    {name:"Barbara Ramos", 
    role:"Graphic Designer", 
-   image:"img/barbare-ramos-graphic-designer.jpg"},
+   image:"barbara-ramos-graphic-designer.jpg"},
       
 ];
 
-// stampiamo le card del team (se ci riusciamo)
+for(let i = 0; i < cards.length; i++){
+   // intero object
+   const singleCard = cards[i]
+   // console.log(singleCard);
+   // nome
+   const nameCard = singleCard.name;
+   console.log(nameCard);
+   // ruolo
+   const roleCard = singleCard.role;
+   console.log(roleCard);
+   // image
+   const imageCard = singleCard.image;
+   console.log(imageCard);
 
-
-
-// funzione per creare nuove card
-
-newCard();
-
-
-function newCard() {
-   // ciclo for per prendere tutti i dati
-   for(let i = 0; i < cards.length; i++){
-      // intero object
-      const singleCard = cards[i]
-      // console.log(singleCard);
-      // nome
-      const nameCard = singleCard.name;
-      console.log(nameCard);
-      // ruolo
-      const roleCard = singleCard.role;
-      console.log(roleCard);
-      // image
-      const imageCard = singleCard.image;
-      console.log(imageCard);
-
-
-      // prendo team-container e ci creo un altro team-card
-      const container = document.querySelector('.team-container');
-      // ci creo un'altro div al suo interno (team-card)
-      const newTeamCard = document.createElement('div');
-      // gli dò la classe team-card
-      newTeamCard.classList.add('team-card');
-      // inserisco newteam card dentro container
-      container.append(newTeamCard);
-      console.log(container);
-      // dentro al div team-card inserisco l'immagine
-      // creo il div card-image con la sua relativa classe
-      const newCardImage = document.createElement('div');
-      // aggiungo classe
-      newCardImage.classList.add('card-image');
-      // inserisco newcard card dentro container
-      newTeamCard.append(newCardImage);
-      console.log(newTeamCard);
-      console.log(newCardImage);
-
-      // inserisco le immagini
-      newCardImage.append(imageCard);
-      
-      // parte testuale
-      const newCardtext = document.createElement('div');
-   }
-
+   newHtml +=`<div class="team-card">
+      <div class="card-image">
+     <img src="img/${imageCard}"
+     alt="${nameCard}"/>
+      </div>
+      <div class="card-text">
+      <h3>${nameCard}</h3>
+      <p>${roleCard}</p>
+      </div>
+      </div>`;
 }
 
-// newCardImage.innerHTML = `<img src="img/${imageCard}"
-//       // alt="${nameCard}"/>`;
+container.innerHTML = newHtml;
+
+   
