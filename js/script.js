7fc8b2e6fee40167e79773a4db747206985b4803
Davide Tavella <tavella01@gmail.com>
2021-11-04 14:29:43 +0100

    array di oggetti dei vari membri

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// 
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// 
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// 
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
+
+
+// creo array di oggetti di ciascun membro
+
+const cards = [
+// membro 2
+   {name:"Angela Carroll", 
+   role:"Chief Editor", 
+   image:"img/angela-caroll-chief-editor.jpg"},
+// membro 3
+   {name:"Walter Gordon", 
+   role:"Office Manager", 
+   image:"img/walter-gordon-office-manager.jpg"},
+// membro 4
+   {name:"Angela Lopez", 
+   role:"Social Media Manager", 
+   image:"img/angela-lopez-social-media-manager.jpg"},
+// membro 5
+   {name:"Scott Estrada", 
+   role:"Developer", 
+   image:"img/scott-estrada-developer.jpg"},
+// membro 6
+   {name:"Barbara Ramos", 
+   role:"Graphic Designer", 
+   image:"img/barbare-ramos-graphic-designer.jpg"},
+      
+
+]
